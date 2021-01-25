1eb6266a85256abe8ef9789baa629101233a94e9
Rudresh Narwal <rudresh.narwal@toffeeinsurance.com>
2021-01-25 23:13:49 +0530

    updated chapter 3

--- Notes APP (File System, Command Line Tool, json parsing and Arrow function basics)/notes-app/app.js
+++ Notes APP (File System, Command Line Tool, json parsing and Arrow function basics)/notes-app/app.js
@@ -2,7 +2,21 @@ const chalk = require('chalk')
 const yargs = require('yargs')
 const notes = require('./notes.js')
 
+
+console.log(process.argv);  /// process is inbuild module in node for interacting with command line arguments
+//Here argv is argument vector
+/*
+Output: [
+  '/usr/local/bin/node',
+  '/Users/apple/dev/node-basics-udemy-course/Notes APP (File System, Command Line Tool, json parsing and Arrow function basics)/notes-app/app.js',
+  'add'
+]
+*/
+
+
 // Customize yargs version
+// yargs provide add on functionality with command line argument interaction
+// you can pass title and body by following command (node app.js add --title="hello" --body="world"
 yargs.version('1.1.0')
 
 // Create add command
