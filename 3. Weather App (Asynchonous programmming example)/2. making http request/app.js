94270686a5fe5c71833a9ac96e1f90776b74ae16
Rudresh Narwal <rudresh.narwal@toffeeinsurance.com>
2021-01-27 12:01:19 +0530

    updated project

---
+++ 3. Weather App (Asynchonous programmming example)/2. making http request/app.js
@@ -0,0 +1,8 @@
+const request = require('request')  //postman-request(alternative)
+
+const url = 'http://api.weatherstack.com/current?access_key=2f830d42ca413ade7b9ed11112cfb47a&query=42.3605,-71.0596'
+
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current);
+})   // this function will run if we have weather data, or there is something wrong in data request
