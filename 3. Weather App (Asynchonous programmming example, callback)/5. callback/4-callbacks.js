079e209dfb180ac11e00cfa58e6348ef8a9317f5
Rudresh Narwal <rudresh.narwal@toffeeinsurance.com>
2021-01-27 16:06:28 +0530

    added weather app

---
+++ 3. Weather App (Asynchonous programmming example, callback)/5. callback/4-callbacks.js
@@ -0,0 +1,53 @@
+// Callback: callback function is a function which we provide
+// as anargument to another function with the intention of having it called later on.
+
+setTimeout(() => {
+    console.log('Two seconds are up')
+}, 2000)
+
+// const names = ['Andrew', 'Jen', 'Jess']
+// const shortNames = names.filter((name) => {
+//     return name.length <= 4
+// })
+
+
+//---- Why we need callback ---////
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         }
+//
+//         return data;
+//     }, 2000)
+// }
+//
+// geocode('Philadelphia');
+
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         }
+//
+//         callback(data)
+//     }, 2000)
+// }
+//
+// geocode('Philadelphia', (data) => {
+//     console.log(data)
+// })
+
+
+//------Example 2------////
+// const add = (a, b, callback) => {
+//     setTimeout(() => {
+//         callback(a + b)
+//     }, 2000)
+// }
+//
+// add(1, 4, (sum) => {
+//     console.log(sum) // Should print: 5
+// })
---
+++ 3. Weather App (Asynchonous programmming example, callback)/8. Es6 Object property(shorthand and destucturing)/Es6 shorthand and destructuring property.js
@@ -0,0 +1,37 @@
+// Object property shorthand  - if you want to define an object who's keys have the same name as the variables
+// passed-in as properties, you can use the shorthand and simply pass the key name.
+
+const name = 'Andrew'
+const userAge = 27
+
+const user = {
+    name,
+    age: userAge,
+    location: 'Philadelphia'
+}
+
+console.log(user)
+
+// Object destructuring -> object destructuring is a useful JavaScript feature to extract properties from objects and bind them to variables.
+
+// const product = {
+//     label: 'Red notebook',
+//     price: 3,
+//     stock: 201,
+//     salePrice: undefined,
+//     rating: 4.2
+// }
+
+// const label = product.label
+// const stock = product.stock
+
+// const {label:productLabel, stock, rating = 5} = product
+// console.log(productLabel)
+// console.log(stock)
+// console.log(rating)
+
+// const transaction = (type, { label, stock }) => {
+//     console.log(type, label, stock)
+// }
+//
+// transaction('order', product)
