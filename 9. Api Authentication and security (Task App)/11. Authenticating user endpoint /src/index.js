4fed62b29359ec63b9b962e9b29cb8313ef88aa6
Rudresh Narwal <rudresh.narwal@toffeeinsurance.com>
2021-05-22 10:21:52 +0530

    added authentication for endpoint

---
+++ 9. Api Authentication and security (Task App)/11. Authenticating user endpoint /src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// we will authenticate user endpoint and change the endpoing for getting user id. see user router
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
---
+++ 9. Api Authentication and security (Task App)/8. Postman basics /readme.txt
@@ -0,0 +1,5 @@
+Check Udemy video :- For managing versioning and many more options of postman
+
+Set Automation in postman -> set auth TOken auto from response
+
+Perform auto test of api
